5c497b6be3af4034fbd5f5883a09f4103d8ea657
realmagus <pedro_eisman@hotmail.com>
2021-10-18 13:28:23 +0200

    fix model structure

--- lib/data/models/stop_model.dart
+++ lib/data/models/stop_model.dart
@@ -34,13 +34,13 @@ class Data {
     required this.extraInfo,
     required this.incident,
   });
-  late final List<Arrive> arrive;
+  late final List<dynamic> arrive;
   late final List<StopInfo> stopInfo;
   late final List<dynamic> extraInfo;
   late final Incident incident;
 
   Data.fromJson(Map<String, dynamic> json) {
-    arrive = List.from(json['Arrive']).map((e) => Arrive.fromJson(e)).toList();
+    arrive = List.castFrom<dynamic, dynamic>(json['Arrive']);
     stopInfo =
         List.from(json['StopInfo']).map((e) => StopInfo.fromJson(e)).toList();
     extraInfo = List.castFrom<dynamic, dynamic>(json['ExtraInfo']);
@@ -49,7 +49,7 @@ class Data {
 
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
-    _data['Arrive'] = arrive.map((e) => e.toJson()).toList();
+    _data['Arrive'] = arrive;
     _data['StopInfo'] = stopInfo.map((e) => e.toJson()).toList();
     _data['ExtraInfo'] = extraInfo;
     _data['Incident'] = incident.toJson();
@@ -57,55 +57,85 @@ class Data {
   }
 }
 
-class Arrive {
-  Arrive({
-    required this.line,
-    required this.stop,
-    required this.isHead,
-    required this.destination,
-    required this.deviation,
-    required this.bus,
+class StopInfo {
+  StopInfo({
+    required this.label,
+    required this.stopLines,
+    required this.description,
     required this.geometry,
-    required this.estimateArrive,
-    required this.distanceBus,
-    required this.positionTypeBus,
+    required this.direction,
   });
-  late final String line;
-  late final String stop;
-  late final String isHead;
-  late final String destination;
-  late final int deviation;
-  late final int bus;
+  late final String label;
+  late final StopLines stopLines;
+  late final String description;
   late final Geometry geometry;
-  late final int estimateArrive;
-  late final int distanceBus;
-  late final String positionTypeBus;
+  late final String direction;
 
-  Arrive.fromJson(Map<String, dynamic> json) {
-    line = json['line'];
-    stop = json['stop'];
-    isHead = json['isHead'];
-    destination = json['destination'];
-    deviation = json['deviation'];
-    bus = json['bus'];
+  StopInfo.fromJson(Map<String, dynamic> json) {
+    label = json['label'];
+    stopLines = StopLines.fromJson(json['StopLines']);
+    description = json['Description'];
     geometry = Geometry.fromJson(json['geometry']);
-    estimateArrive = json['estimateArrive'];
-    distanceBus = json['DistanceBus'];
-    positionTypeBus = json['positionTypeBus'];
+    direction = json['Direction'];
   }
 
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
-    _data['line'] = line;
-    _data['stop'] = stop;
-    _data['isHead'] = isHead;
-    _data['destination'] = destination;
-    _data['deviation'] = deviation;
-    _data['bus'] = bus;
+    _data['label'] = label;
+    _data['StopLines'] = stopLines.toJson();
+    _data['Description'] = description;
     _data['geometry'] = geometry.toJson();
-    _data['estimateArrive'] = estimateArrive;
-    _data['DistanceBus'] = distanceBus;
-    _data['positionTypeBus'] = positionTypeBus;
+    _data['Direction'] = direction;
+    return _data;
+  }
+}
+
+class StopLines {
+  StopLines({
+    required this.data,
+  });
+  late final List<LineData> data;
+
+  StopLines.fromJson(Map<String, dynamic> json) {
+    data = List.from(json['Data']).map((e) => LineData.fromJson(e)).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['Data'] = data.map((e) => e.toJson()).toList();
+    return _data;
+  }
+}
+
+class LineData {
+  LineData({
+    required this.label,
+    required this.line,
+    required this.description,
+    required this.distance,
+    required this.to,
+  });
+  late final String label;
+  late final String line;
+  late final String description;
+  late final int distance;
+  late final String to;
+
+  LineData.fromJson(Map<String, dynamic> json) {
+    label = json['Label'];
+    line = json['line'];
+    description = json['Description'];
+    distance = json['distance'];
+    to = json['to'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final _data = <String, dynamic>{};
+    _data['Label'] = label;
+    _data['line'] = line;
+    _data['Description'] = description;
+    _data['distance'] = distance;
+    _data['to'] = to;
     return _data;
   }
 }
@@ -116,11 +146,11 @@ class Geometry {
     required this.coordinates,
   });
   late final String type;
-  late final List<int> coordinates;
+  late final List<double> coordinates;
 
   Geometry.fromJson(Map<String, dynamic> json) {
     type = json['type'];
-    coordinates = List.castFrom<dynamic, int>(json['coordinates']);
+    coordinates = List.castFrom<dynamic, double>(json['coordinates']);
   }
 
   Map<String, dynamic> toJson() {
@@ -131,110 +161,13 @@ class Geometry {
   }
 }
 
-class StopInfo {
-  StopInfo({
-    required this.lines,
-    required this.stopId,
-    required this.stopName,
-    required this.geometry,
-    required this.direction,
-  });
-  late final List<Lines> lines;
-  late final String stopId;
-  late final String stopName;
-  late final Geometry geometry;
-  late final String direction;
-
-  StopInfo.fromJson(Map<String, dynamic> json) {
-    lines = List.from(json['lines']).map((e) => Lines.fromJson(e)).toList();
-    stopId = json['stopId'];
-    stopName = json['stopName'];
-    geometry = Geometry.fromJson(json['geometry']);
-    direction = json['Direction'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['lines'] = lines.map((e) => e.toJson()).toList();
-    _data['stopId'] = stopId;
-    _data['stopName'] = stopName;
-    _data['geometry'] = geometry.toJson();
-    _data['Direction'] = direction;
-    return _data;
-  }
-}
-
-class Lines {
-  Lines({
-    required this.label,
-    required this.line,
-    required this.nameA,
-    required this.nameB,
-    required this.metersFromHeader,
-    required this.to,
-    required this.color,
-  });
-  late final String label;
-  late final String line;
-  late final String nameA;
-  late final String nameB;
-  late final int metersFromHeader;
-  late final String to;
-  late final String color;
-
-  Lines.fromJson(Map<String, dynamic> json) {
-    label = json['label'];
-    line = json['line'];
-    nameA = json['nameA'];
-    nameB = json['nameB'];
-    metersFromHeader = json['metersFromHeader'];
-    to = json['to'];
-    color = json['color'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['label'] = label;
-    _data['line'] = line;
-    _data['nameA'] = nameA;
-    _data['nameB'] = nameB;
-    _data['metersFromHeader'] = metersFromHeader;
-    _data['to'] = to;
-    _data['color'] = color;
-    return _data;
-  }
-}
-
 class Incident {
-  Incident({
-    required this.listaIncident,
-  });
-  late final ListaIncident listaIncident;
+  Incident();
 
-  Incident.fromJson(Map<String, dynamic> json) {
-    listaIncident = ListaIncident.fromJson(json['ListaIncident']);
-  }
+  Incident.fromJson(Map json);
 
   Map<String, dynamic> toJson() {
     final _data = <String, dynamic>{};
-    _data['ListaIncident'] = listaIncident.toJson();
-    return _data;
-  }
-}
-
-class ListaIncident {
-  ListaIncident({
-    required this.data,
-  });
-  late final List<Data> data;
-
-  ListaIncident.fromJson(Map<String, dynamic> json) {
-    data = List.from(json['data']).map((e) => Data.fromJson(e)).toList();
-  }
-
-  Map<String, dynamic> toJson() {
-    final _data = <String, dynamic>{};
-    _data['data'] = data.map((e) => e.toJson()).toList();
     return _data;
   }
 }
