6e782dd5f4d23bbd2c8799966b81947926abceaf
realmagus <pedro_eisman@hotmail.com>
2021-10-19 19:37:25 +0200

    add AppTheme class

---
+++ lib/views/theme/theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const TextStyle waitingTime = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 16.0,
+  );
+
+  static const TextStyle errorMessage = TextStyle(
+    fontSize: 18.0,
+  );
+
+  static const TextStyle lineNumber = TextStyle(
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+    fontSize: 20.0,
+  );
+}
