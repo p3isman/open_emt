6d3ad4260e6973517fdb283dde143e5739c3a520
realmagus <pedro_eisman@hotmail.com>
2021-10-20 12:26:37 +0200

    add poppins font and waitingTimeSecondary style

--- lib/views/theme/theme.dart
+++ lib/views/theme/theme.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static const TextStyle waitingTime = TextStyle(
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+    fontSize: 16.0,
+  );
+
+  static const TextStyle waitingTimeSecondary = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Poppins',
     fontSize: 16.0,
   );
 
