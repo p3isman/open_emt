f203d590cf7c57dc7100939264a05f94beef159d
realmagus <pedro_eisman@hotmail.com>
2021-10-21 17:51:52 +0200

    create favorites repo to access database

---
+++ lib/domain/repositories/favorites_repository.dart
@@ -0,0 +1,27 @@
+import 'package:open_emt/data/models/stop_model.dart';
+import 'package:open_emt/data/services/db_service.dart';
+
+class FavoritesRepository {
+  final DBService dbService;
+
+  const FavoritesRepository({required this.dbService});
+
+  /// Adds a quote to database
+  Future<void> addToFavorites(StopInfo stop) async {
+    await dbService.addFavorite(stop);
+  }
+
+  /// Deletes a quote from database
+  Future<void> deleteFavorite(StopInfo stop) async {
+    await dbService.deleteFavorite(stop);
+  }
+
+  /// Deletes all quotes from database and widget list
+  Future<void> deleteAllFavorites() async {
+    await dbService.deleteAllFavorites();
+  }
+
+  Future<List<StopInfo>> getAllFavorites() async {
+    return await dbService.getAllFavorites();
+  }
+}
