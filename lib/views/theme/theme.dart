05834e7bed973f06b3f07f6e8c0b5df6b76226b8
realmagus <pedro_eisman@hotmail.com>
2021-10-25 10:46:58 +0200

    add a text style for the title

--- lib/views/theme/theme.dart
+++ lib/views/theme/theme.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static const TextStyle title = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+    fontSize: 22.0,
+  );
+
   static const TextStyle waitingTime = TextStyle(
     fontWeight: FontWeight.w500,
     fontFamily: 'Poppins',
