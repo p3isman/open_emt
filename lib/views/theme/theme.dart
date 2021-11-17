47275718bd812a9f80ac9bb40a15a60a640dc862
p3isman <pedro_eisman@hotmail.com>
2021-11-17 11:37:36 +0100

    Add styles to app bar and bottom bar Create a light and dark styles to use them on different screens

--- lib/views/theme/theme.dart
+++ lib/views/theme/theme.dart
@@ -12,6 +12,26 @@ class AppTheme {
     ),
   );
 
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: const ColorScheme.dark(
+      primary: Colors.blue,
+      secondary: Colors.lightBlue,
+    ),
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.white38,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+    ),
+    switchTheme: SwitchThemeData(
+      thumbColor: MaterialStateProperty.all(Colors.white),
+      trackColor: MaterialStateProperty.all(Colors.black87),
+    ),
+  );
+
   static const TextStyle title = TextStyle(
     fontWeight: FontWeight.w500,
     fontFamily: 'Poppins',
@@ -44,5 +64,50 @@ class AppTheme {
     color: Colors.white,
     fontWeight: FontWeight.bold,
     fontSize: 20.0,
+  );
+
+  static final appBarLightFlexibleSpace = Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: <Color>[
+          Colors.blue,
+          Colors.blue.shade900,
+        ],
+      ),
+    ),
+  );
+
+  static final appBarDarkFlexibleSpace = Container(
+    decoration: BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: <Color>[
+          Colors.black,
+          Colors.grey.shade900,
+        ],
+      ),
+    ),
+  );
+
+  static final bottomBarDecorationLight = BoxDecoration(
+    gradient: LinearGradient(
+      colors: [Colors.blue, Colors.blue.shade900],
+      begin: Alignment.topLeft,
+      end: Alignment.topRight,
+      tileMode: TileMode.clamp,
+    ),
+  );
+
+  static final bottomBarDecorationDark = BoxDecoration(
+    gradient: LinearGradient(
+      colors: [Colors.black, Colors.grey.shade900],
+      begin: Alignment.topLeft,
+      end: Alignment.topRight,
+      stops: const [0.0, 0.8],
+      tileMode: TileMode.clamp,
+    ),
   );
 }
