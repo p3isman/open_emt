2deac97ff115d05926328a4fda936ae8141dd9c3
p3isman <pedro_eisman@hotmail.com>
2021-11-18 16:20:25 +0100

    Remove top-level BlocBuilder

--- lib/views/screens/home_screen/tabs/home_tab.dart
+++ lib/views/screens/home_screen/tabs/home_tab.dart
@@ -22,90 +22,86 @@ class HomeTab extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
 
-    return BlocBuilder<StopInfoBloc, StopInfoState>(
-      builder: (context, state) {
-        return ListView(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20.0,
-                horizontal: 10.0,
-              ),
-              child: TextFormField(
-                key: _formFieldKey,
-                controller: _textEditingController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                    labelText: 'Número de parada',
-                    hintText: 'Introduce un código de parada.',
-                    border: OutlineInputBorder(),
-                    icon: FaIcon(FontAwesomeIcons.bus)),
-                validator: (text) {
-                  if (text!.isEmpty) {
-                    return 'Introduce un número de parada.';
-                  }
-                  text = text.trim();
-                  if (text == '' ||
-                      int.parse(text) is! int ||
-                      int.parse(text) <= 0) {
-                    return 'Parada no válida.';
-                  }
-                },
-                onFieldSubmitted: (text) async {
-                  if (_formFieldKey.currentState!.validate()) {
-                    context.read<StopInfoBloc>().add(GetStopInfo(stopId: text));
-                    FocusScope.of(context).unfocus();
-                    await Navigator.pushNamed(context, DetailScreen.route,
-                        arguments: ScreenArguments(stopId: text));
-                  }
-                },
-              ),
-            ),
-            BlocBuilder<FavoriteStopsBloc, FavoriteStopsState>(
-                builder: (context, state) {
-              if (state is FavoritesLoadSuccess) {
-                return state.stops.isEmpty
-                    ? Column(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 15.0,
-                              vertical: 40.0,
-                            ),
-                            child: Text(
-                                'Marca paradas como favoritas para verlas aquí',
-                                style: AppTheme.title),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Padding(
-                              padding: EdgeInsets.only(right: size.width * 0.1),
-                              child: SvgPicture.asset(
-                                'assets/bus.svg',
-                                width: size.width * 0.55,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    : Column(
-                        children: List.generate(
-                          state.stops.length,
-                          (index) => Column(
-                            children: [
-                              if (index != 0) const Divider(),
-                              FavoriteStop(state, index),
-                            ],
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 20.0,
+            horizontal: 10.0,
+          ),
+          child: TextFormField(
+            key: _formFieldKey,
+            controller: _textEditingController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+                labelText: 'Número de parada',
+                hintText: 'Introduce un código de parada.',
+                border: OutlineInputBorder(),
+                icon: FaIcon(FontAwesomeIcons.bus)),
+            validator: (text) {
+              if (text!.isEmpty) {
+                return 'Introduce un número de parada.';
+              }
+              text = text.trim();
+              if (text == '' ||
+                  int.parse(text) is! int ||
+                  int.parse(text) <= 0) {
+                return 'Parada no válida.';
+              }
+            },
+            onFieldSubmitted: (text) async {
+              if (_formFieldKey.currentState!.validate()) {
+                context.read<StopInfoBloc>().add(GetStopInfo(stopId: text));
+                FocusScope.of(context).unfocus();
+                await Navigator.pushNamed(context, DetailScreen.route,
+                    arguments: ScreenArguments(stopId: text));
+              }
+            },
+          ),
+        ),
+        BlocBuilder<FavoriteStopsBloc, FavoriteStopsState>(
+            builder: (context, state) {
+          if (state is FavoritesLoadSuccess) {
+            return state.stops.isEmpty
+                ? Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 15.0,
+                          vertical: 40.0,
+                        ),
+                        child: Text(
+                            'Marca paradas como favoritas para verlas aquí',
+                            style: AppTheme.title),
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.only(right: size.width * 0.1),
+                          child: SvgPicture.asset(
+                            'assets/bus.svg',
+                            width: size.width * 0.55,
                           ),
                         ),
-                      );
-              } else {
-                return const SizedBox.shrink();
-              }
-            })
-          ],
-        );
-      },
+                      ),
+                    ],
+                  )
+                : Column(
+                    children: List.generate(
+                      state.stops.length,
+                      (index) => Column(
+                        children: [
+                          if (index != 0) const Divider(),
+                          FavoriteStop(state, index),
+                        ],
+                      ),
+                    ),
+                  );
+          } else {
+            return const SizedBox.shrink();
+          }
+        })
+      ],
     );
   }
 }
