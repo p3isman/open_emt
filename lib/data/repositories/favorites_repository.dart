21b7d792e8c08cb5cb77e4d4176e79f99e253035
p3isman <pedro_eisman@hotmail.com>
2021-11-18 16:20:01 +0100

    Add update method

--- lib/data/repositories/favorites_repository.dart
+++ lib/data/repositories/favorites_repository.dart
@@ -13,11 +13,9 @@ class FavoritesRepository implements IFavoritesRepository {
 
   /// Database getter
   Future<Database?> get database async {
-    // If database exists return it
+    // Singleton
     if (_database != null) return _database;
-
-    _database = await initDB();
-    return _database;
+    return await initDB();
   }
 
   /// Creates a table for the quotes in case it doesn't exist
@@ -28,10 +26,7 @@ class FavoritesRepository implements IFavoritesRepository {
     // Creates a database
     return await openDatabase(
       _path,
-
-      // If newer version, onCreate will be called
-      version: 2,
-
+      version: 2, // If newer version, onCreate will be called
       onOpen: (Database db) {},
       onCreate: (Database db, int version) async {
         await db.execute(
@@ -41,21 +36,18 @@ class FavoritesRepository implements IFavoritesRepository {
   }
 
 // INSERTS
+  /// Returns ID of the last inserted row
   @override
   Future<int> addToFavorites(StopInfo stopInfo) async {
     final db = await database;
-    final res =
-        await db!.insert('Favorites', {'stop': jsonEncode(stopInfo.toJson())});
-
-    /// ID of the last inserted row
-    return res;
+    return await db!
+        .insert('Favorites', {'stop': jsonEncode(stopInfo.toJson())});
   }
 
 // SELECTS
   @override
   Future<List<StopInfo>> getAllFavorites() async {
     final db = await database;
-
     final res = await db!.query('Favorites', columns: ['stop']);
 
     return res.isNotEmpty
@@ -66,21 +58,32 @@ class FavoritesRepository implements IFavoritesRepository {
         : [];
   }
 
+// UPDATES
+  @override
+  Future<int> updateFavorite(
+    StopInfo oldStop,
+    StopInfo updatedStop,
+  ) async {
+    final db = await database;
+    return await db!.update(
+      'Favorites',
+      {'stop': jsonEncode(updatedStop)},
+      where: 'stop = ?',
+      whereArgs: [jsonEncode(oldStop)],
+    );
+  }
+
 // DELETES
   @override
   Future<int> deleteFavorite(StopInfo stop) async {
     final db = await database;
-    final res = await db!
+    return await db!
         .delete('Favorites', where: 'stop = ?', whereArgs: [jsonEncode(stop)]);
-
-    return res;
   }
 
   @override
   Future<int> deleteAllFavorites() async {
     final db = await database;
-    final res = await db!.delete('Favorites');
-
-    return res;
+    return await db!.delete('Favorites');
   }
 }
--- lib/domain/repositories/i_favorites_repository.dart
+++ lib/domain/repositories/i_favorites_repository.dart
@@ -1,26 +1,21 @@
 import 'package:open_emt/data/models/stop_model.dart';
 
 class IFavoritesRepository {
-  /// Adds a quote to database
+  /// Adds a stop to database
   Future<void> addToFavorites(StopInfo stop) async {}
 
-  /// Deletes a quote from database
+  /// Updates a stop
+  Future<void> updateFavorite(
+    StopInfo oldStop,
+    StopInfo updatedStop,
+  ) async {}
+
+  /// Deletes a stop from database
   Future<void> deleteFavorite(StopInfo stop) async {}
 
-  /// Deletes all quotes from database and widget list
+  /// Deletes all stops from database and widget list
   Future<void> deleteAllFavorites() async {}
 
   /// Returns all favorite stops
   Future<List<StopInfo>?> getAllFavorites() async {}
-
-  // bool checkIfFavorite(
-  //   List<StopInfo> favorites,
-  //   StopInfo stop,
-  // ) {
-  //   for (var i in favorites) {
-  //     if (i.label == stop.label) return true;
-  //   }
-
-  //   return false;
-  // }
 }
