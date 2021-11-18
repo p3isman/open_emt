07c6be235bb4dcb3a94639ae8a7102cc05b6b8fa
p3isman <pedro_eisman@hotmail.com>
2021-11-18 16:21:39 +0100

    Small format fix

--- lib/views/screens/detail_screen/widgets/arrive_info.dart
+++ lib/views/screens/detail_screen/widgets/arrive_info.dart
@@ -12,113 +12,115 @@ class ArriveInfoWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 3.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
+      elevation: 3.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  margin: const EdgeInsets.only(
+                    top: 10.0,
+                    left: 15.0,
+                    bottom: 10.0,
+                  ),
+                  elevation: 4.0,
+                  child: Container(
+                    decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    margin: const EdgeInsets.only(
-                      top: 10.0,
-                      left: 15.0,
-                      bottom: 10.0,
-                    ),
-                    elevation: 4.0,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        gradient: LinearGradient(
-                          colors: arriveInfo.first.line[0] == 'N'
-                              ? [Colors.black, Colors.grey.shade700]
-                              : [Colors.blue.shade700, Colors.blue.shade400],
-                        ),
+                      gradient: LinearGradient(
+                        colors: arriveInfo.first.line[0] == 'N'
+                            ? [Colors.black, Colors.grey.shade700]
+                            : [Colors.blue.shade700, Colors.blue.shade400],
                       ),
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 15.0, vertical: 8.0),
-                        child: Text(
-                          arriveInfo.first.line,
-                          style: AppTheme.lineNumber,
-                        ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 8.0),
+                      child: Text(
+                        arriveInfo.first.line,
+                        style: AppTheme.lineNumber,
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                            children: arriveInfo[0].estimateArrive < 30
-                                ? [
-                                    const Text(
-                                      '<<<',
-                                      style: AppTheme.waitingTime,
-                                    )
-                                  ]
-                                : [
-                                    Text(
-                                      '${(arriveInfo[0].estimateArrive / 60).round()}min',
-                                      style: AppTheme.waitingTime,
-                                    ),
-                                    const Text(
-                                      ' | ',
-                                      style: AppTheme.waitingTime,
-                                    ),
-                                    Text(
-                                      '${arriveInfo[0].distanceBus}m',
-                                      style: AppTheme.waitingTime,
-                                    ),
-                                  ]),
-                        const SizedBox(height: 5.0),
-                        Row(
-                            children: arriveInfo[1].estimateArrive < 30
-                                ? [
-                                    const Text(
-                                      '<<<',
-                                      style: AppTheme.waitingTimeSecondary,
-                                    )
-                                  ]
-                                : [
-                                    Text(
-                                      '${(arriveInfo[1].estimateArrive / 60).round()}min',
-                                      style: AppTheme.waitingTimeSecondary,
-                                    ),
-                                    const Text(
-                                      ' | ',
-                                      style: AppTheme.waitingTimeSecondary,
-                                    ),
-                                    Text(
-                                      '${arriveInfo[1].distanceBus}m',
-                                      style: AppTheme.waitingTimeSecondary,
-                                    ),
-                                  ]),
-                      ],
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                          children: arriveInfo[0].estimateArrive < 30
+                              ? [
+                                  const Text(
+                                    '<<<',
+                                    style: AppTheme.waitingTime,
+                                  )
+                                ]
+                              : [
+                                  Text(
+                                    '${(arriveInfo[0].estimateArrive / 60).round()}min',
+                                    style: AppTheme.waitingTime,
+                                  ),
+                                  const Text(
+                                    ' | ',
+                                    style: AppTheme.waitingTime,
+                                  ),
+                                  Text(
+                                    '${arriveInfo[0].distanceBus}m',
+                                    style: AppTheme.waitingTime,
+                                  ),
+                                ]),
+                      const SizedBox(height: 5.0),
+                      Row(
+                        children: arriveInfo[1].estimateArrive < 30
+                            ? [
+                                const Text(
+                                  '<<<',
+                                  style: AppTheme.waitingTimeSecondary,
+                                )
+                              ]
+                            : [
+                                Text(
+                                  '${(arriveInfo[1].estimateArrive / 60).round()}min',
+                                  style: AppTheme.waitingTimeSecondary,
+                                ),
+                                const Text(
+                                  ' | ',
+                                  style: AppTheme.waitingTimeSecondary,
+                                ),
+                                Text(
+                                  '${arriveInfo[1].distanceBus}m',
+                                  style: AppTheme.waitingTimeSecondary,
+                                ),
+                              ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Text(
+                    arriveInfo.first.destination.toLowerCase().toTitleCase(),
+                    style: AppTheme.waitingTime,
                   ),
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Text(
-                      arriveInfo.first.destination.toLowerCase().toTitleCase(),
-                      style: AppTheme.waitingTime,
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        ));
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
